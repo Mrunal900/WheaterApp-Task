71d48491c983d8a1c58bb7caea45a3c335c40c71
Mrunal900 <mrunalwaghdhare786@gmail.com>
2024-03-24 20:28:37 +0530

    first commit

---
+++ wheatherApp.py
@@ -0,0 +1,39 @@
+import requests
+
+
+def get_weather(city_name):
+  # API key should be kept secure and not shared in public.
+    api_key = 'API_KEY'
+    base_url = "https://api.openweathermap.org/data/2.5/weather?"
+
+    url = f"{base_url}q={city_name}&appid={api_key}&units=metric"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        # Parse the JSON response
+        data = response.json()
+
+
+        city = data['name']
+        temperature = data['main']['temp']
+        description = data['weather'][0]['description']
+
+        return {'city': city, 'temperature': temperature, 'description': description}
+
+    except (requests.RequestException) as error:
+        print(f"Error fetching weather data for {city_name}: {error}")
+        return None
+
+
+
+if __name__ == "__main__":
+    city_name = input("Enter City Name: ")
+    weather_data = get_weather(city_name)
+    if weather_data:
+        print(f"Current weather in {weather_data['city']}:")
+        print(f"Temperature: {weather_data['temperature']}°C")
+        print(f"Description: {weather_data['description']}")
+    else:
+        print("Failed to retrieve weather data.")
